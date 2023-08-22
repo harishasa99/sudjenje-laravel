b4d1ec3beba73a12e7059337098c292d5bc8b07a
harishasa99 <harishasa99@gmail.com>
2023-08-22 15:19:26 +0200

    edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -127,9 +127,7 @@
     </div>
 
     
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script> -->
+   
 
 
 
